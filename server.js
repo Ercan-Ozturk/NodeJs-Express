783065cdbebbe78935736b33445e005f128134d1
ercan-ozturk <ercanozturk788@gmail.com>
2024-09-03 15:58:48 +0300

    express setup

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
-});
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/api/posts", (req, res) => {});
 app.listen(8000, () => {
   console.log("Server is running on port 5000");
 });
