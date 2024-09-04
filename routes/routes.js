942e55b8133dc8ecbb71052e20a61e443a1ff9fe
Ercan Öztürk <ercanozturk788@gmail.com>
2024-09-04 12:48:52 +0300

    Express routing

---
+++ routes/routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+  { id: 4, title: "Post Four" },
+];
+
+router.get("/api/posts", (req, res) => {
+  const limit = parseInt(req.query.limit);
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+  res.status(200).json(posts);
+});
+
+router.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  res.status(200).json(post);
+});
--- server.js
+++ server.js
@@ -6,25 +6,7 @@ const app = express();
 
 //app.use(express.static(path.join(__dirname, "public")));
 ("");
-let posts = [
-  { id: 1, title: "Post One" },
-  { id: 2, title: "Post Two" },
-  { id: 3, title: "Post Three" },
-  { id: 4, title: "Post Four" },
-];
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-  if (!isNaN(limit) && limit > 0) {
-    res.json(posts.slice(0, limit));
-  } else {
-    res.json(posts);
-  }
-});
 
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  res.json(posts.filter((post) => post.id === id));
-});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
