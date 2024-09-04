c249e24369e8d2974792072612702b24608ca6ae
Ercan Öztürk <ercanozturk788@gmail.com>
2024-09-04 13:00:56 +0300

    Commonjs to modules switch

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const path = require("path");
-const posts = require("./routes/posts");
-const port = process.env.PORT || 8000;
+import express from("express");
+import path from("path");
+import posts from("./routes/posts");
+import port from process.env.PORT || 8000;
 
 const app = express();
 
